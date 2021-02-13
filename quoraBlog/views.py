7b1b9b9e2e920f71440b902312fddc3aae65fea3
virajdoshi11 <viraj.nd@somaiya.edu>
2021-02-13 19:45:23 +0530

    added 2 pages (kind of)

--- quoraBlog/views.py
+++ quoraBlog/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(req):
-    return HttpResponse('<h1>Somaiya Quora</h1>')
+    return render(req, 'homePage.html', {})
+    
+def about(req):
+    return render(req, 'about.html', {})
--- somaiyaQuora/urls.py
+++ somaiyaQuora/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('quoraBlog/', include('quoraBlog.urls')),
+    path('', include('quoraBlog.urls')),
 ]
