d94b833189a8bb0a9a5639fafa95f79aad0a1954
virajdoshi11 <viraj.nd@somaiya.edu>
2021-02-15 16:02:36 +0530

    added base template

--- quoraBlog/views.py
+++ quoraBlog/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author': 'Abc',
+        'title': 'Post 1',
+        'content': 'This is the first post',
+        'date': '14 Feb, 2021',
+    },
+    {
+        'author': 'Xyz',
+        'title': 'Post 2',
+        'content': 'Hey Abc are you single ?',
+        'date': 'Yes',
+    }
+]
+
 def home(req):
-    return render(req, 'homePage.html', {})
+    context = {
+        'keyPosts': posts,
+        'title': 'Qoura',
+    }
+    return render(req, 'homePage.html', context)
     
 def about(req):
     return render(req, 'about.html', {})
