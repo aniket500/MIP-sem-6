e7e1eaad1dead7e66fb22a0dc0a885b9e949638c
Vartika G <vartikag.2424@gmail.com>
2021-02-22 15:50:34 +0530

    templates added

--- somaiyaQuora/settings.py
+++ somaiyaQuora/settings.py
@@ -31,13 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'quoraBlog.apps.QuorablogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'quoraBlog',
+    # 'quoraBlog',
 ]
 
 MIDDLEWARE = [
