618df7d962f3facb963de7714ae506b43e26fdac
Aniket Joshi <aniket05@somaiya.edu>
2021-03-17 14:53:01 +0530

    made changes

--- quoraBlog/urls.py
+++ quoraBlog/urls.py
@@ -4,12 +4,12 @@ from users import views as user_views
 from .views import PostCreateView
 
 urlpatterns = [
-    path('', views.login_page, name='login_page'),
-    path('home/', views.home, name='home'),
+    path('login/', views.login_page, name='login_page'),
+    path('', views.home, name='home'),
     path('profile/',views.profile, name='profile'),
     path('register/', user_views.register, name='register'),
     path('test/', views.login, name='login'),
     path('txt_sum/', views.txt_sum, name='txt_sum'),
-    path('post/new/', PostCreateView.as_view(), name='post-create')
-    #path('blogs/', views.blogs, name='blogs'),
+    #path('post/new/', PostCreateView.as_view(), name='post-create')
+    path('blogs/', views.blogs, name='blogs'),
 ]
