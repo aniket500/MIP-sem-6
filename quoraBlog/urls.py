4958e44befb7b84c3159af0a44c7b391631fef5b
Vartika G <vartikag.2424@gmail.com>
2021-03-24 12:41:42 +0530

    commit "new post UI done"

--- quoraBlog/urls.py
+++ quoraBlog/urls.py
@@ -14,5 +14,5 @@ urlpatterns = [
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
-    #path('blogs/', views.blogs, name='blogs'),
+    path('blogs/', views.blogs, name='blogs'),
 ]
